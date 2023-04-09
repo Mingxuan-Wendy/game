b929b56ab2080abaa6894e86bf0dc5f9037a13f8
heweiwei <945247329@qq.com>
2023-04-09 10:05:41 +0800

    左边菜单栏简易版

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import Router from 'vue-router'
+import Home from '../views/home.vue'
+// Vue.prototype.$message = Message
 
 Vue.use(Router)
 
-export default new Router({
-    routes:[
-        {
-            path:'/',
-            // 路由懒加载
-            // component:() => import('@/components/Home')
-        }
-    ],
-    mode:'hash'
+// export default new Router({
+//     routes:[
+//         {
+//             path:'/',
+//             // 路由懒加载
+//             // component:() => import('@/components/Home')
+//         }
+//     ],
+//     mode:'hash'
+// })
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component:Home
+    },
+]
+
+const router = new VueRouter({
+    routes
 })
+export default router
---
+++ src/views/home.vue
@@ -0,0 +1,106 @@
+<template>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <span>LOGO</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu background-color="#fff" text-color="#000">
+          <el-menu-item index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>Home</span>
+            </template>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">Collection</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-document"></i>
+            <span slot="title">Friends</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Invite</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Shopping</span>
+          </el-menu-item>
+          <el-menu-item index="6">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Community</span>
+          </el-menu-item>
+          <el-menu-item index="7">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Settings</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #000;
+  font-size: 20px;
+}
+.el-header div {
+  display: flex;
+  align-items: center;
+}
+
+.el-header div span {
+  margin-left: 15px;
+}
+
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.el-menu-item:hover {
+  background-color: #6e5bd9 !important;
+  color: #fff !important;
+}
+.el-menu-item:hover i {
+  color: #fff !important;
+}
+
+.el-menu-item.is-active {
+  background-color: #6e5bd9 !important;
+  color: #fff !important;
+}
+.el-menu-item.is-active i {
+  color: #fff !important;
+}
+</style>
